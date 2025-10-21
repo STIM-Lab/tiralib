1661f69ffaee603716654dfa4bd3eaaefa512ecf
David <mayerich@uh.edu>
2025-10-21 17:12:32 -0500

    added notes for the vascular.py file

---
+++ python/vascular.py
@@ -0,0 +1,8 @@
+# internal structure of the vascular class:
+    
+    # nodes: an Nx3 array of node positions
+    # edges: an Sx2 array of nodes connecting each edge
+    # radii: an Sx1 array of radii for each node
+
+def func():
+    print("define the vascular class here and add some basic functions for generating simple vasc files")
